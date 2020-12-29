46e7c1b7735a7dadb179573ae516966f04fe7c5e
pramit-marattha <pramitmarattha5497@gmail.com>
2020-12-30 01:18:04 +0545

    readme added

--- scripts/dataFunctions.js
+++ scripts/dataFunctions.js
@@ -12,7 +12,7 @@ export const retrieveSearchResults = async (searchTerm) =>{
 
 const getWikiSearchString = (searchTerm) => {
     const maxCharacter = getMaxCharacter();
-    const rawSearchString =  `https://en.wikipedia.org/w/api.php?action=query&generator=search&gsrsearch=${searchTerm}&gsrlimit=20&prop=pageimages|extracts&exchars=${maxCharactergit}&exintro&explaintext&exlimit=max&format=json&origin=*`;
+    const rawSearchString =  `https://en.wikipedia.org/w/api.php?action=query&generator=search&gsrsearch=${searchTerm}&gsrlimit=20&prop=pageimages|extracts&exchars=${maxCharacter}&exintro&explaintext&exlimit=max&format=json&origin=*`;
 }
 
 const getMaxCharacter =() =>{
